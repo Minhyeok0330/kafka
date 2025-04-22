9842f9fefeb55003797e0bf5aa5ec49410ad8622
Minhyeok0330 <alsgurgud@naver.com>
2025-04-22 14:59:32 +0900

    spark so hard

--- log-producer.py
+++ log-producer.py
@@ -21,7 +21,7 @@ def generate_log_line(timestamp):
     status_code = random.choice([200, 301, 400, 404, 500])
     response_size = random.randint(200, 5000)
 
-    return f'{ip} [{timestamp}] "{method} {path} {protocol}" {status_code} {response_size}'
+    return f'{ip} {timestamp} {method} {path} {protocol} {status_code} {response_size}'
 
 def send_one_log():
     now = datetime.now().strftime('%Y-%m-%d:%H:%M:%S')
