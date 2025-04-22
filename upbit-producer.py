a35c0ff9b1ee592b208d7aeaae7ab9bd198c4c8b
Minhyeok0330 <alsgurgud@naver.com>
2025-04-22 13:36:39 +0900

    consumer error fix

--- upbit-producer.py
+++ upbit-producer.py
@@ -1,10 +1,25 @@
 from confluent_kafka import Producer
+import time
+import requests
+
+UPBIT_URL = 'https://api.upbit.com/v1/ticker'
+params = {
+    'markets': 'KRW-BTC'
+}
 
 conf = {
     'bootstrap.servers': 'localhost:9092',
 }
 p = Producer(conf)
 
-p.poll(0)
-p.produce('temp', 'python-message-test')
-p.flush()
+while True:
+    res = requests.get(UPBIT_URL, params=params)
+    bit_data = res.json()[0]
+    result = f"{bit_data['market']}, {bit_data['trade_date']}, {bit_data['trade_time']}, {bit_data['trade_price']}"
+    p.poll(0)
+    p.produce('temp', result)
+    p.flush()
+    
+
+    time.sleep(5)
+    
